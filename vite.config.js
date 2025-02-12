a8f1f50047ed67e17dbbf7ff53352740db85b012
Harkanav <kanavbadwal@gmail.com>
2025-02-12 18:09:12 +0530

    fix/deploy app

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://www.greatermohali.com/",
+  base: "main/",
   resolve: {
     extensions: [".js", ".jsx"],
   },
   server: {
+    port: 3000,
+    open: true,
     headers: {
       "Content-Type": "application/javascript",
+    },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
     },
   },
 });
