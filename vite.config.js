2b4392015a1643957ea21759d4273ec09fc10a97
Harkanav <kanavbadwal@gmail.com>
2025-02-16 10:41:47 +0530

    optimisations

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-    alias: {
-      "@": "/src",
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
 });
