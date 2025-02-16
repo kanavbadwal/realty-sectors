aec50869b624d39ca21e301be31a2cc5e891ed6a
Harkanav <kanavbadwal@gmail.com>
2025-02-16 12:04:03 +0530

    fix/changes according to prod.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
+  },
+  server: {
+    port: 3000,
   },
 });
